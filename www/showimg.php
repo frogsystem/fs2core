d9ab31b07649b6153474138e044940368cc172b2
Sweil <Sweil@b49b0359-402e-0410-ad19-9fd90e479030>
2008-09-01 20:09:00 +0000

    git-svn-id: http://svn.xp-dev.com/svn/Sweil_fs2/src@97 b49b0359-402e-0410-ad19-9fd90e479030

--- www/showimg.php
+++ www/showimg.php
@@ -1,8 +1,10 @@
 <?php
-include("config.inc.php");
+include("login.inc.php");
 if ($db)
 {
     include("includes/functions.php");
+    include("includes/indexfunctions.php");
+    include("includes/imagefunctions.php");
 
 /////////////////////////////
 //// Konstruktor aufrufe ////
@@ -48,7 +50,8 @@ if (isset($_GET[screen]))
 {
     $index = mysql_query("SELECT * FROM ".$global_config_arr[pref]."screen WHERE screen_id = $_GET[screenid]", $db);
     $_GET[title] = mysql_result($index, 0, "screen_name");
-    $_GET[pic] = image_url("images/screenshots/", $_GET[screenid]);
+	$pic = image_url("images/screenshots/", $_GET[screenid], TRUE, TRUE);
+	$url = image_url("images/screenshots/", $_GET[screenid]);
 
     // gibt es ein nächstes Bild?
     $index = mysql_query("SELECT * FROM ".$global_config_arr[pref]."screen
@@ -86,28 +89,26 @@ if (isset($_GET[screen]))
 $max_width = $config_arr[show_img_x];
 $max_height = $config_arr[show_img_y];
 
-$_GET[pic_url] = $_GET[pic];
-
-list($width, $height) = getimagesize($_GET[pic]);
+list($width, $height) = getimagesize($pic);
 $imgratio=$width/$height;
 if ($width<=$max_width AND $height<=$max_height)
 {
-  $_GET[pic] = "<img src='$_GET[pic]' border='0' alt='$_GET[title]'>";
+  $pic = "<img src='$url' border='0' alt='$_GET[title]'>";
 }
 elseif ($imgratio>1)   //Querformat
 {
   if ($max_width/$imgratio > $max_height)
   {
-    $_GET[pic] = "<img src='$_GET[pic]' height='$max_height' border='0' alt='$_GET[title]'>";
+    $pic = "<img src='$url' height='$max_height' border='0' alt='$_GET[title]'>";
   }
   else
   {
-    $_GET[pic] = "<img src='$_GET[pic]' width='$max_width' border='0' alt='$_GET[title]'>";
+    $pic = "<img src='$url' width='$max_width' border='0' alt='$_GET[title]'>";
   }
 }
 else    //Hochformat
 {
-  $_GET[pic] = "<img src='$_GET[pic]' height='$max_height' border='0' alt='$_GET[title]'>";
+  $pic = "<img src='$url' height='$max_height' border='0' alt='$_GET[title]'>";
 }
 
 $close ="&nbsp;&nbsp;&nbsp;<a href='#ank' onclick='self.close();'>[Fenster schlie&szlig;en]</a>&nbsp;&nbsp;&nbsp;";
@@ -118,8 +119,8 @@ $template_viewer = stripslashes(mysql_result($index, 0, "pic_viewer"));
 $template_viewer = str_replace("{text}", $_GET[title], $template_viewer);
 $template_viewer = str_replace("{weiter_grafik}", $next, $template_viewer);
 $template_viewer = str_replace("{zurück_grafik}", $prev, $template_viewer);
-$template_viewer = str_replace("{bild}", $_GET[pic], $template_viewer);
-$template_viewer = str_replace("{bild_url}", $_GET[pic_url], $template_viewer);
+$template_viewer = str_replace("{bild}", $pic, $template_viewer);
+$template_viewer = str_replace("{bild_url}", $url, $template_viewer);
 $template_viewer = str_replace("{close}", $close, $template_viewer);
 
 
