efa63e2c6d629ea86652998df51f71258d22fba0
Thoronador <thoronador@users.sourceforge.net>
2012-04-20 01:00:55 +0200

    pedantic fixes in search-related admin CP scripts

--- www/admin/admin_search_config.php
+++ www/admin/admin_search_config.php
@@ -1,13 +1,13 @@
-<?php if (!defined("ACP_GO")) die("Unauthorized access!");
+<?php if (!defined('ACP_GO')) die('Unauthorized access!');
 
 #TODO: 
     
 ###################
 ## Page Settings ##
 ###################
-$search_cols = array("id", "search_num_previews", "search_and", "search_or", "search_xor", "search_not", "search_wildcard", "search_min_word_length", "search_allow_phonetic", "search_use_stopwords");
-$global_cols = array("search_index_update");
-      
+$search_cols = array('id', 'search_num_previews', 'search_and', 'search_or', 'search_xor', 'search_not', 'search_wildcard', 'search_min_word_length', 'search_allow_phonetic', 'search_use_stopwords');
+$global_cols = array('search_index_update');
+
 
 ///////////////////////
 //// Update Config ////
@@ -30,9 +30,9 @@ if (
  
      // save to db
     try {
-        $sql->save("search_config", $search);
-        $FD->saveConfig("main", $global);
-        systext($FD->text("admin", "changes_saved"), $FD->text("admin", "info"), "green", $FD->text("admin", "icon_save_ok"));
+        $sql->save('search_config', $search);
+        $FD->saveConfig('main', $global);
+        systext($FD->text('admin', 'changes_saved'), $FD->text('admin', 'info'), 'green', $FD->text('admin', 'icon_save_ok'));
     } catch (Exception $e) {}
     
     // Unset Vars
@@ -47,14 +47,14 @@ if ( TRUE )
 {
     // Display Error Messages
     if (isset($_POST['sended'])) {
-        systext($FD->text("admin", "changes_not_saved").'<br>'.$FD->text("admin", "form_not_filled"), $FD->text("admin", "error"), "red", $FD->text("admin", "icon_save_error"));
+        systext($FD->text('admin', 'changes_not_saved').'<br>'.$FD->text('admin', 'form_not_filled'), $FD->text('admin', 'error'), 'red', $FD->text('admin', 'icon_save_error'));
 
     // Load Data from DB into Post
     } else {
-        $search = $sql->getRow("search_config", $search_cols, array('W' => "`id` = 1"));
-        $global = $sql->getRow("global_config", $global_cols, array('W' => "`id` = 1"));
+        $search = $sql->getRow('search_config', $search_cols, array('W' => '`id` = 1'));
+        $global = $sql->getRow('global_config', $global_cols, array('W' => '`id` = 1'));
         
-        $global = $sql->getRow("config", array("config_data"), array('W' => "`config_name` = 'main'"));
+        $global = $sql->getRow('config', array('config_data'), array('W' => "`config_name` = 'main'"));
         $global = json_array_decode($global['config_data']);
         $global = array_filter_keys($global, $global_cols);
         
@@ -63,13 +63,13 @@ if ( TRUE )
     }   
     
     // security functions
-    $_POST = array_map("killhtml", $_POST);    
-     
+    $_POST = array_map('killhtml', $_POST);
+
     // Conditions
-    $adminpage->addCond("search_allow_phonetic", $_POST['search_allow_phonetic'] == 1);
-    $adminpage->addCond("search_use_stopwords", $_POST['search_use_stopwords'] == 1);
+    $adminpage->addCond('search_allow_phonetic', $_POST['search_allow_phonetic'] == 1);
+    $adminpage->addCond('search_use_stopwords', $_POST['search_use_stopwords'] == 1);
     for ($i=1;$i<=3;$i++)
-        $adminpage->addCond("search_index_update_".$i, $_POST['search_index_update'] == $i);  
+        $adminpage->addCond('search_index_update_'.$i, $_POST['search_index_update'] == $i);
     
     // Values
     foreach ($_POST as $key => $value) {
@@ -77,7 +77,7 @@ if ( TRUE )
     }
 
     // Display page
-    echo $adminpage->get("main");
+    echo $adminpage->get('main');
 }
 
 ?>
--- www/admin/admin_search_index.php
+++ www/admin/admin_search_index.php
@@ -1,49 +1,49 @@
-<?php if (!defined("ACP_GO")) die("Unauthorized access!");
+<?php if (!defined('ACP_GO')) die('Unauthorized access!');
 
 //////////////////////
 //// Do Index Ops ////
 //////////////////////
-if ( isset ( $_POST['do'] ) && ( in_array ( $_POST['do'], array ( "update", "new", "delete", "delete_with_word" ) ) ) ) {
+if ( isset ( $_POST['do'] ) && ( in_array ( $_POST['do'], array ( 'update', 'new', 'delete', 'delete_with_word' ) ) ) ) {
 
     // Include searchfunctions.php
-    require ( FS2_ROOT_PATH . "includes/searchfunctions.php" );
+    require ( FS2_ROOT_PATH . 'includes/searchfunctions.php' );
 
     // Update
-    if ( $_POST['do'] == "update" ) {
-        update_search_index ( "news" );
-        update_search_index ( "articles" );
-        update_search_index ( "dl" );
+    if ( $_POST['do'] == 'update' ) {
+        update_search_index ( 'news' );
+        update_search_index ( 'articles' );
+        update_search_index ( 'dl' );
         // Display Message
-        systext ( $FD->text("page", "search_index_updated"),
-            $FD->text("page", "info"), FALSE, $FD->text("page", "icon_save_ok") );
+        systext ( $FD->text('page', 'search_index_updated'),
+            $FD->text('page', 'info'), FALSE, $FD->text('page', 'icon_save_ok') );
 
     // New
-    } elseif ( $_POST['do'] == "new" ) {
-        new_search_index ( "news" );
-        new_search_index ( "articles" );
-        new_search_index ( "dl" );
+    } elseif ( $_POST['do'] == 'new' ) {
+        new_search_index ( 'news' );
+        new_search_index ( 'articles' );
+        new_search_index ( 'dl' );
         // Display Message
-        systext ( $FD->text("page", "search_index_renewed"),
-            $FD->text("page", "info"), FALSE, $FD->text("page", "icon_save_add") );
+        systext ( $FD->text('page', 'search_index_renewed'),
+            $FD->text('page', 'info'), FALSE, $FD->text('page', 'icon_save_add') );
 
     // Delete
-    } elseif ( $_POST['do'] == "delete" ) {
-        delete_search_index ( "news" );
-        delete_search_index ( "articles" );
-        delete_search_index ( "dl" );
+    } elseif ( $_POST['do'] == 'delete' ) {
+        delete_search_index ( 'news' );
+        delete_search_index ( 'articles' );
+        delete_search_index ( 'dl' );
         // Display Message
-        systext ( $FD->text("page", "search_index_deleted"),
-            $FD->text("page", "info"), FALSE, $FD->text("page", "icon_trash_ok") );
+        systext ( $FD->text('page', 'search_index_deleted'),
+            $FD->text('page', 'info'), FALSE, $FD->text('page', 'icon_trash_ok') );
     
     // Delete with word List
-    } elseif ( $_POST['do'] == "delete_with_word" ) {
-        delete_search_index ( "news" );
-        delete_search_index ( "articles" );
-        delete_search_index ( "dl" );
+    } elseif ( $_POST['do'] == 'delete_with_word' ) {
+        delete_search_index ( 'news' );
+        delete_search_index ( 'articles' );
+        delete_search_index ( 'dl' );
         delete_word_list();
         // Display Message
-        systext ( $FD->text("page", "search_index_deleted_with_word_list"),
-            $FD->text("page", "info"), FALSE, $FD->text("page", "icon_trash_ok") );
+        systext ( $FD->text('page', 'search_index_deleted_with_word_list'),
+            $FD->text('page', 'info'), FALSE, $FD->text('page', 'icon_trash_ok') );
     }
 }
 
@@ -54,84 +54,84 @@ if ( isset ( $_POST['do'] ) && ( in_array ( $_POST['do'], array ( "update", "new
 if ( TRUE )
 {
     // Get some Data
-    $words = mysql_query ( "SELECT COUNT(`search_word_id`) AS 'words' FROM `".$global_config_arr['pref']."search_words`", $FD->sql()->conn() );
-    $docs = mysql_query ( "SELECT COUNT(`search_time_id`) AS 'docs' FROM `".$global_config_arr['pref']."search_time`", $FD->sql()->conn() );
+    $words = mysql_query ( "SELECT COUNT(`search_word_id`) AS 'words' FROM `".$global_config_arr['pref'].'search_words`', $FD->sql()->conn() );
+    $docs = mysql_query ( "SELECT COUNT(`search_time_id`) AS 'docs' FROM `".$global_config_arr['pref'].'search_time`', $FD->sql()->conn() );
 
     // Display Form
     echo '
                         <table class="configtable" cellpadding="4" cellspacing="0">
-                            <tr><td class="line" colspan="2">'.$FD->text("page", "search_index_title").'</td></tr>
+                            <tr><td class="line" colspan="2">'.$FD->text('page', 'search_index_title').'</td></tr>
                             <tr>
                                 <td class="config right_space">
-                                    '.$FD->text("page", "search_index_update_title").':<br>
-                                    <span class="small">'.$FD->text("page", "search_index_update_desc").'</span>
+                                    '.$FD->text('page', 'search_index_update_title').':<br>
+                                    <span class="small">'.$FD->text('page', 'search_index_update_desc').'</span>
                                 </td>
                                 <td class="config center">
                                     <form action="" method="post">
                                         <input type="hidden" name="go" value="search_index">
                                         <input type="hidden" name="do" value="update">
-                                        <input class="button input_width" type="submit" value="'.$FD->text("page", "search_index_update_button").'">
+                                        <input class="button input_width" type="submit" value="'.$FD->text('page', 'search_index_update_button').'">
                                     </form>
                                 </td>
                             </tr>
                             <tr>
                                 <td class="config right_space">
-                                    '.$FD->text("page", "search_index_new_title").':<br>
-                                    <span class="small">'.$FD->text("page", "search_index_new_desc").'</span>
+                                    '.$FD->text('page', 'search_index_new_title').':<br>
+                                    <span class="small">'.$FD->text('page', 'search_index_new_desc').'</span>
                                 </td>
                                 <td class="config center">
                                     <form action="" method="post">
                                         <input type="hidden" name="go" value="search_index">
                                         <input type="hidden" name="do" value="new">
-                                        <input class="button input_width" type="submit" value="'.$FD->text("page", "search_index_new_button").'"><br>
-                                        <span class="small">'.$FD->text("page", "search_index_new_note").'</span>
+                                        <input class="button input_width" type="submit" value="'.$FD->text('page', 'search_index_new_button').'"><br>
+                                        <span class="small">'.$FD->text('page', 'search_index_new_note').'</span>
                                     </form>
                                 </td>
                             </tr>
                             <tr>
                                 <td class="config right_space">
-                                    '.$FD->text("page", "search_index_delete_title").':<br>
-                                    <span class="small">'.$FD->text("page", "search_index_delete_desc").'</span>
+                                    '.$FD->text('page', 'search_index_delete_title').':<br>
+                                    <span class="small">'.$FD->text('page', 'search_index_delete_desc').'</span>
                                 </td>
                                 <td class="config center">
                                     <form action="" method="post">
                                         <input type="hidden" name="go" value="search_index">
                                         <input type="hidden" name="do" value="delete">
-                                        <input class="button input_width" type="submit" value="'.$FD->text("page", "search_index_delete_button").'"><br>
-                                        <span class="small">'.$FD->text("page", "search_index_delete_note").'</span>
+                                        <input class="button input_width" type="submit" value="'.$FD->text('page', 'search_index_delete_button').'"><br>
+                                        <span class="small">'.$FD->text('page', 'search_index_delete_note').'</span>
                                     </form>
                                 </td>
                             </tr>
                             <tr>
                                 <td class="config right_space">
-                                    '.$FD->text("page", "search_index_delete_with_word_title").':<br>
-                                    <span class="small">'.$FD->text("page", "search_index_delete_with_word_desc").'</span>
+                                    '.$FD->text('page', 'search_index_delete_with_word_title').':<br>
+                                    <span class="small">'.$FD->text('page', 'search_index_delete_with_word_desc').'</span>
                                 </td>
                                 <td class="config center">
                                     <form action="" method="post">
                                         <input type="hidden" name="go" value="search_index">
                                         <input type="hidden" name="do" value="delete_with_word">
-                                        <input class="button input_width" type="submit" value="'.$FD->text("page", "search_index_delete_with_word_button").'"><br>
-                                        <span class="small">'.$FD->text("page", "search_index_delete_with_word_note").'</span>
+                                        <input class="button input_width" type="submit" value="'.$FD->text('page', 'search_index_delete_with_word_button').'"><br>
+                                        <span class="small">'.$FD->text('page', 'search_index_delete_with_word_note').'</span>
                                     </form>
                                 </td>
                             </tr>
                             <tr><td class="space"></td></tr>
-                            <tr><td class="line" colspan="2">'.$FD->text("page", "search_index_info_title").'</td></tr>
+                            <tr><td class="line" colspan="2">'.$FD->text('page', 'search_index_info_title').'</td></tr>
                             <tr>
                                 <td class="config right_space">
-                                    '.$FD->text("page", "search_index_info_words").':
+                                    '.$FD->text('page', 'search_index_info_words').':
                                 </td>
                                 <td class="configthin left">
-                                    '.mysql_result ( $words, 0, "words" ).'
+                                    '.mysql_result ( $words, 0, 'words' ).'
                                 </td>
                             </tr>
                             <tr>
                                 <td class="config right_space">
-                                    '.$FD->text("page", "search_index_info_docs").':
+                                    '.$FD->text('page', 'search_index_info_docs').':
                                 </td>
                                 <td class="configthin left">
-                                    '.mysql_result ( $docs, 0, "docs" ).'
+                                    '.mysql_result ( $docs, 0, 'docs' ).'
                                 </td>
                             </tr>
                         </table>
