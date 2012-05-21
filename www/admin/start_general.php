f8aa9a7f7c5080192405a4f02f6ed6607a8f1edc
Thoronador <thoronador@users.sourceforge.net>
2012-05-22 00:59:11 +0200

    fixed causes of some PHP notices in start scripts of admin CP pages + some more pedantic stuff

--- www/admin/start_general.php
+++ www/admin/start_general.php
@@ -1,65 +1,65 @@
-<?php if ( ACP_GO === "start_general" ) {
+<?php if ( ACP_GO === 'start_general' ) {
     
 // Online since
-$online_since = $sql->getRow("counter_stat", array("s_year", "s_month", "s_day"), array('O' => "`s_year`, `s_month`, `s_day`"));
-$online_since = date_loc($global_config_arr['date'], strtotime($online_since['s_year']."-".$online_since['s_month']."-".$online_since['s_day']));
+$online_since = $sql->getRow('counter_stat', array('s_year', 's_month', 's_day'), array('O' => '`s_year`, `s_month`, `s_day`'));
+$online_since = date_loc($global_config_arr['date'], strtotime($online_since['s_year'].'-'.$online_since['s_month'].'-'.$online_since['s_day']));
 
 // Total
-$total = $sql->getRow("counter", array("visits", "hits"));
+$total = $sql->getRow('counter', array('visits', 'hits'));
 
 // Today
-$today = $sql->getRow("counter_stat", array("s_hits", "s_visits"), array('W' => "`s_year` = '".$global_config_arr['env']['year']."' AND `s_month` = '".(int) $global_config_arr['env']['month']."' AND `s_day` = '".(int) $global_config_arr['env']['day']."'"));
+$today = $sql->getRow('counter_stat', array('s_hits', 's_visits'), array('W' => "`s_year` = '".$global_config_arr['env']['year']."' AND `s_month` = '".(int) $global_config_arr['env']['month']."' AND `s_day` = '".(int) $global_config_arr['env']['day']."'"));
 
 
 // Visitors online
 $online['visitors'] = $sql->getField(
-    "useronline",
-    array('COL' => "user_id", 'FUNC' => "COUNT", 'AS' => "visitors_on")
+    'useronline',
+    array('COL' => 'user_id', 'FUNC' => 'COUNT', 'AS' => 'visitors_on')
 );
 
 
 // Users online
-$index = $sql->doQuery("SELECT count(user_id) AS user_on FROM {..pref..}useronline WHERE user_id != 0");
-$online['users'] = mysql_result($index, 0, "user_on");
+$index = $sql->doQuery('SELECT count(user_id) AS user_on FROM {..pref..}useronline WHERE user_id != 0');
+$online['users'] = mysql_result($index, 0, 'user_on');
 
 // Guests online
 $online['guests'] = $online['visitors'] - $online['users'];
 
 // Referrer Num
 $index = $sql->doQuery("SELECT COUNT(`ref_url`) AS 'ref_num' FROM {..pref..}counter_ref");
-$ref['num'] = mysql_result($index, 0, "ref_num");
+$ref['num'] = mysql_result($index, 0, 'ref_num');
 
 if ($ref['num'] > 0) {
 	// last Ref
-	$index = $sql->doQuery("SELECT ref_url, ref_last FROM {..pref..}counter_ref ORDER BY ref_last DESC LIMIT 0,1");
-	$ref['url'] = stripslashes(mysql_result($index, 0, "ref_url"));
-	$ref['shorturl'] = cut_in_string($ref['url'], 50, "...");
+	$index = $sql->doQuery('SELECT ref_url, ref_last FROM {..pref..}counter_ref ORDER BY ref_last DESC LIMIT 0,1');
+	$ref['url'] = stripslashes(mysql_result($index, 0, 'ref_url'));
+	$ref['shorturl'] = cut_in_string($ref['url'], 50, '...');
 
-	$ref['date_time'] = date_loc($TEXT['admin']->get("date_time"), mysql_result($index, 0, "ref_last"));
+	$ref['date_time'] = date_loc($TEXT['admin']->get('date_time'), mysql_result($index, 0, 'ref_last'));
 }
 
 // Conditions
-$adminpage->addCond("ref_link",($ref['num'] > 0));
-$adminpage->addCond("hans", true);
-$adminpage->addCond("wurst", false);
-$adminpage->addCond("thomas", false);
+$adminpage->addCond('ref_link',($ref['num'] > 0));
+$adminpage->addCond('hans', true);
+$adminpage->addCond('wurst', false);
+$adminpage->addCond('thomas', false);
 
 // Values
-$adminpage->addText("title", $global_config_arr['title']);
-$adminpage->addText("url", $global_config_arr['virtualhost']);
-$adminpage->addText("online_since", $online_since);
-$adminpage->addText("online_visitors", $online['visitors']);
-$adminpage->addText("online_guests", $online['guests']);
-$adminpage->addText("online_registered", $online['users']);
-$adminpage->addText("visits_total", $total['visits']);
-$adminpage->addText("visits_today", $today['s_visits']);
-$adminpage->addText("hits_total", $total['hits']);
-$adminpage->addText("hits_today", $today['s_hits']);
-$adminpage->addText("num_of_refs", $ref['num']);
-$adminpage->addText("ref_url", $ref['url']);
-$adminpage->addText("ref_shorturl", $ref['shorturl']);
-$adminpage->addText("ref_date", $ref['date_time']);
+$adminpage->addText('title', $global_config_arr['title']);
+$adminpage->addText('url', $global_config_arr['virtualhost']);
+$adminpage->addText('online_since', $online_since);
+$adminpage->addText('online_visitors', $online['visitors']);
+$adminpage->addText('online_guests', $online['guests']);
+$adminpage->addText('online_registered', $online['users']);
+$adminpage->addText('visits_total', $total['visits']);
+$adminpage->addText('visits_today', $today['s_visits']);
+$adminpage->addText('hits_total', $total['hits']);
+$adminpage->addText('hits_today', $today['s_hits']);
+$adminpage->addText('num_of_refs', $ref['num']);
+$adminpage->addText('ref_url', $ref['url']);
+$adminpage->addText('ref_shorturl', $ref['shorturl']);
+$adminpage->addText('ref_date', $ref['date_time']);
 
-echo $adminpage->get("main");
+echo $adminpage->get('main');
 
 } ?>
--- www/admin/start_system.php
+++ www/admin/start_system.php
@@ -1,68 +1,68 @@
 <?php
 $index = mysql_query ( "
                         SELECT COUNT(`alias_id`) AS 'num_aliases'
-                        FROM `".$global_config_arr['pref']."aliases`
-", $FD->sql()->conn() );
-$num_aliases = mysql_result ( $index, 0, "num_aliases" );
+                        FROM `".$global_config_arr['pref'].'aliases`
+', $FD->sql()->conn() );
+$num_aliases = mysql_result ( $index, 0, 'num_aliases' );
 $index = mysql_query ( "
                         SELECT COUNT(`alias_id`) AS 'num_aliases_active'
-                        FROM `".$global_config_arr['pref']."aliases`
+                        FROM `".$global_config_arr['pref'].'aliases`
                         WHERE `alias_active` = 1
-", $FD->sql()->conn() );
-$num_aliases_active = mysql_result ( $index, 0, "num_aliases_active" );
+', $FD->sql()->conn() );
+$num_aliases_active = mysql_result ( $index, 0, 'num_aliases_active' );
 
 $index = mysql_query ( "
                         SELECT COUNT(`applet_id`) AS 'num_applets'
-                        FROM `".$global_config_arr['pref']."applets`
-", $FD->sql()->conn() );
-$num_applets = mysql_result ( $index, 0, "num_applets" );
+                        FROM `".$global_config_arr['pref'].'applets`
+', $FD->sql()->conn() );
+$num_applets = mysql_result ( $index, 0, 'num_applets' );
 $index = mysql_query ( "
                         SELECT COUNT(`applet_id`) AS 'num_applets_active'
-                        FROM `".$global_config_arr['pref']."applets`
+                        FROM `".$global_config_arr['pref'].'applets`
                         WHERE `applet_active` = 1
-", $FD->sql()->conn() );
-$num_applets_active = mysql_result ( $index, 0, "num_applets_active" );
+', $FD->sql()->conn() );
+$num_applets_active = mysql_result ( $index, 0, 'num_applets_active' );
 
 
 $index = mysql_query ( "
                         SELECT COUNT(`snippet_id`) AS 'num_snippets'
-                        FROM `".$global_config_arr['pref']."snippets`
-", $FD->sql()->conn() );
-$num_snippets = mysql_result ( $index, 0, "num_snippets" );
+                        FROM `".$global_config_arr['pref'].'snippets`
+', $FD->sql()->conn() );
+$num_snippets = mysql_result ( $index, 0, 'num_snippets' );
 $index = mysql_query ( "
                         SELECT COUNT(`snippet_id`) AS 'num_snippets_active'
-                        FROM `".$global_config_arr['pref']."snippets`
+                        FROM `".$global_config_arr['pref'].'snippets`
                         WHERE `snippet_active` = 1
-", $FD->sql()->conn() );
-$num_snippets_active = mysql_result ( $index, 0, "num_snippets_active" );
+', $FD->sql()->conn() );
+$num_snippets_active = mysql_result ( $index, 0, 'num_snippets_active' );
 
 
 echo '
                         <table class="configtable" cellpadding="4" cellspacing="0">
-                            <tr><td class="line" colspan="2">'.$TEXT["admin"]->get("informations_and_statistics").'</td></tr>
+                            <tr><td class="line" colspan="2">'.$TEXT['admin']->get('informations_and_statistics').'</td></tr>
                             <tr>
-                                <td class="configthin" width="200">'.$TEXT["admin"]->get("system_start_num_aliases").':</td>
+                                <td class="configthin" width="200">'.$TEXT['admin']->get('system_start_num_aliases').':</td>
                                 <td class="configthin"><b>'.$num_aliases.'</b></td>
                             </tr>
                             <tr>
-                                <td class="configthin" width="200">'.$TEXT["admin"]->get("system_start_num_aliases_active").':</td>
+                                <td class="configthin" width="200">'.$TEXT['admin']->get('system_start_num_aliases_active').':</td>
                                 <td class="configthin"><b>'.$num_aliases_active.'</b></td>
                             </tr>
                             <tr><td class="space"></td></tr>
                             <tr>
-                                <td class="configthin">'.$TEXT["admin"]->get("system_start_num_applets").':</td>
+                                <td class="configthin">'.$TEXT['admin']->get('system_start_num_applets').':</td>
                                 <td class="configthin"><b>'.$num_applets.'</b></td>
                             </tr>
                             <tr>
-                                <td class="configthin">'.$TEXT["admin"]->get("system_start_num_applets_active").':</td>
+                                <td class="configthin">'.$TEXT['admin']->get('system_start_num_applets_active').':</td>
                                 <td class="configthin"><b>'.$num_applets_active.'</b></td>
                             </tr>
                             <tr><td class="space"></td></tr>
                             <tr>
-                                <td class="configthin">'.$TEXT["admin"]->get("system_start_num_snippets").':</td>
+                                <td class="configthin">'.$TEXT['admin']->get('system_start_num_snippets').':</td>
                                 <td class="configthin"><b>'.$num_snippets.'</b></td>
                             </tr>
-                                <td class="configthin">'.$TEXT["admin"]->get("system_start_num_snippets_active").':</td>
+                                <td class="configthin">'.$TEXT['admin']->get('system_start_num_snippets_active').':</td>
                                 <td class="configthin"><b>'.$num_snippets_active.'</b></td>
                             </tr>
                         </table>
