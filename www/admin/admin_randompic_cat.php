182d39e6d8f5aa3111a649532b9354706db691d2
Thoronador <thoronador@users.sourceforge.net>
2012-05-22 23:33:42 +0200

    fixed causes of some PHP notices in randompic-related admin CP scripts; added/improved input sanitizing to avoid SQL injections

--- www/admin/admin_randompic_cat.php
+++ www/admin/admin_randompic_cat.php
@@ -8,11 +8,13 @@ if ($_POST['sended'])
 {
     while (list($key, $val) = each($_POST['randompic_cat']))
     {
-        mysql_query("UPDATE ".$global_config_arr[pref]."screen_cat
+        $key = intval($key); //better be safe than sorry
+        $val = intval($val);
+        mysql_query('UPDATE '.$global_config_arr['pref']."screen_cat
                      SET randompic = '$val'
                      WHERE cat_id = '$key'", $FD->sql()->conn() );
     }
-    systext("Einstellungen wurden gespeichert!");
+    systext('Einstellungen wurden gespeichert!');
 }
 
 ////////////////////////
@@ -41,41 +43,41 @@ if ($_POST['sended'])
                                 </td>
                             </tr>
     ';
-    $index = mysql_query("select * from ".$global_config_arr[pref]."screen_cat WHERE cat_type != 2 order by cat_name asc", $FD->sql()->conn() );
+    $index = mysql_query('SELECT * FROM '.$global_config_arr['pref'].'screen_cat WHERE cat_type != 2 ORDER BY cat_name ASC', $FD->sql()->conn() );
     while ($cat_arr = mysql_fetch_assoc($index))
     {
-        $cat_arr[cat_date] = date("d.m.Y", $cat_arr[cat_date]);
-        $screen_index = mysql_query("select cat_id from ".$global_config_arr[pref]."screen where cat_id = $cat_arr[cat_id]", $FD->sql()->conn() );
+        $cat_arr['cat_date'] = date('d.m.Y', $cat_arr['cat_date']);
+        $screen_index = mysql_query('SELECT cat_id FROM '.$global_config_arr['pref']."screen where cat_id = $cat_arr[cat_id]", $FD->sql()->conn() );
         $screen_rows = mysql_num_rows($screen_index);
         echo'
-                            <input type="hidden" name="randompic_cat['.$cat_arr[cat_id].']" value="0">
+                            <input type="hidden" name="randompic_cat['.$cat_arr['cat_id'].']" value="0">
                             <tr>
                                 <td class="configthin">
-                                    '.$cat_arr[cat_name].'
+                                    '.$cat_arr['cat_name'].'
                                 </td>
                                 <td class="configthin">
                                     '.$screen_rows.'
                                 </td>
                                 <td class="configthin">';
-                                    switch ($cat_arr[cat_visibility]) {
+                                    switch ($cat_arr['cat_visibility']) {
                                     case 0:
-                                        echo "Nicht aufrufbar";
+                                        echo 'Nicht aufrufbar';
                                         break;
                                     case 1:
-                                        echo "Sichtbar";
+                                        echo 'Sichtbar';
                                         break;
                                     case 2:
-                                       echo "Nicht in Auswahl";
+                                       echo 'Nicht in Auswahl';
                                        break;
                                     }
                                     echo'
                                 </td>
                                 <td class="configthin">
-                                    '.$cat_arr[cat_date].'
+                                    '.$cat_arr['cat_date'].'
                                 </td>
                                 <td class="configthin">
-                                    <input type="checkbox" name="randompic_cat['.$cat_arr[cat_id].']" value="1"';
-                                        if ($cat_arr[randompic] == 1)
+                                    <input type="checkbox" name="randompic_cat['.$cat_arr['cat_id'].']" value="1"';
+                                        if ($cat_arr['randompic'] == 1)
                                           echo ' checked=checked';
                                         echo'
                                     >
--- www/admin/admin_randompic_time_add.php
+++ www/admin/admin_randompic_time_add.php
@@ -5,17 +5,17 @@
 $startdate = null;
 $enddate   = null;
 if (!empty($_POST['screen_id'])) {
-    $startdate = mktime($_POST[nowstunde], $_POST[nowmin], 0, $_POST[nowmonat], $_POST[nowtag], $_POST[nowjahr]);
-    $enddate = mktime($_POST[endstunde], $_POST[endmin], 0, $_POST[endmonat], $_POST[endtag], $_POST[endjahr]);
+    $startdate = mktime($_POST['nowstunde'], $_POST['nowmin'], 0, $_POST['nowmonat'], $_POST['nowtag'], $_POST['nowjahr']);
+    $enddate = mktime($_POST['endstunde'], $_POST['endmin'], 0, $_POST['endmonat'], $_POST['endtag'], $_POST['endjahr']);
 }
 if ($startdate < $enddate) {
     settype($_POST['screen_id'], 'integer');
-    mysql_query("INSERT INTO ".$global_config_arr[pref]."screen_random (screen_id, start, end)
+    mysql_query('INSERT INTO '.$global_config_arr['pref']."screen_random (screen_id, start, end)
                  VALUES ('". $_POST['screen_id'] ."',
                          '". $startdate ."',
                          '". $enddate ."'
                         )", $FD->sql()->conn() );
-    systext("Zeitgesteuertes Zufallsbild wurde hinzugefügt");
+    systext('Zeitgesteuertes Zufallsbild wurde hinzugef&uuml;gt');
 }
 /////////////////////////////
 //// Screenshot Formular ////
@@ -24,29 +24,29 @@ else
 {
 
     //Zeit-Array für Jetzt Button
-    $jetzt[time] = time();
-    $jetzt[tag] = date("d", $jetzt[time]);
-    $jetzt[monat] = date("m", $jetzt[time]);
-    $jetzt[jahr] = date("Y", $jetzt[time]);
-    $jetzt[stunde] = date("H", $jetzt[time]);
-    $jetzt[minute] = date("i", $jetzt[time]);
+    $jetzt['time'] = time();
+    $jetzt['tag'] = date('d', $jetzt['time']);
+    $jetzt['monat'] = date('m', $jetzt['time']);
+    $jetzt['jahr'] = date('Y', $jetzt['time']);
+    $jetzt['stunde'] = date('H', $jetzt['time']);
+    $jetzt['minute'] = date('i', $jetzt['time']);
 
-    if (!$_POST[sended])
+    if (!$_POST['sended'])
     {
-        $_POST[nowtag] = $jetzt[tag];
-        $_POST[nowmonat] = $jetzt[monat];
-        $_POST[nowjahr] = $jetzt[jahr];
-        $_POST[nowstunde] = $jetzt[stunde];
-        $_POST[nowmin] = $jetzt[minute];
+        $_POST['nowtag'] = $jetzt['tag'];
+        $_POST['nowmonat'] = $jetzt['monat'];
+        $_POST['nowjahr'] = $jetzt['jahr'];
+        $_POST['nowstunde'] = $jetzt['stunde'];
+        $_POST['nowmin'] = $jetzt['minute'];
 
-        $end = $jetzt[time] + 7*24*60*60;
-        $_POST[endtag] = date("d", $end);
-        $_POST[endmonat] = date("m", $end);
-        $_POST[endjahr] = date("Y", $end);
-        $_POST[endstunde] = date("H", $end);
-        $_POST[endmin] = date("i", $end);
+        $end = $jetzt['time'] + 7*24*60*60;
+        $_POST['endtag'] = date('d', $end);
+        $_POST['endmonat'] = date('m', $end);
+        $_POST['endjahr'] = date('Y', $end);
+        $_POST['endstunde'] = date('H', $end);
+        $_POST['endmin'] = date('i', $end);
     }
-    
+
     echo'
                     <form action="" enctype="multipart/form-data" method="post">
                         <input type="hidden" value="timedpic_add" name="go">
@@ -55,7 +55,7 @@ else
                             <tr>
                                 <td class="config" valign="top" width="160">
                                     Bild:<br>
-                                    <font class="small">Bild auswählen</font>
+                                    <font class="small">Bild ausw&auml;hlen</font>
                                 </td>
                                 <td valign="top" width="240">
                                     <input type="button" class="button" value="Bild ausw&auml;hlen" onClick=\'open("admin_findpicture.php","Bild","width=360,height=300,screenX=50,screenY=50,scrollbars=YES")\'">
@@ -72,17 +72,17 @@ else
                                     <font class="small">Bild soll angezeigt werden ab</font>
                                 </td>
                                 <td class="config" valign="top" colspan="2">
-                                    <input id="startday" class="text" size="1" value="'.$_POST[nowtag].'" name="nowtag" maxlength="2"> .
-                                    <input id="startmonth" class="text" size="1" value="'.$_POST[nowmonat].'" name="nowmonat" maxlength="2"> .
-                                    <input id="startyear" class="text" size="3" value="'.$_POST[nowjahr].'" name="nowjahr" maxlength="4">
+                                    <input id="startday" class="text" size="1" value="'.$_POST['nowtag'].'" name="nowtag" maxlength="2"> .
+                                    <input id="startmonth" class="text" size="1" value="'.$_POST['nowmonat'].'" name="nowmonat" maxlength="2"> .
+                                    <input id="startyear" class="text" size="3" value="'.$_POST['nowjahr'].'" name="nowjahr" maxlength="4">
                                     <font class="small"> um </font>
-                                    <input id="starthour" class="text" size="1" value="'.$_POST[nowstunde].'" name="nowstunde" maxlength="2"> :
-                                    <input id="startminute" class="text" size="1" value="'.$_POST[nowmin].'" name="nowmin" maxlength="2"> Uhr
-                                    <input onClick=\'document.getElementById("startday").value="'.$jetzt[tag].'";
-                                                     document.getElementById("startmonth").value="'.$jetzt[monat].'";
-                                                     document.getElementById("startyear").value="'.$jetzt[jahr].'";
-                                                     document.getElementById("starthour").value="'.$jetzt[stunde].'";
-                                                     document.getElementById("startminute").value="'.$jetzt[minute].'";\' class="button" type="button" value="Jetzt">
+                                    <input id="starthour" class="text" size="1" value="'.$_POST['nowstunde'].'" name="nowstunde" maxlength="2"> :
+                                    <input id="startminute" class="text" size="1" value="'.$_POST['nowmin'].'" name="nowmin" maxlength="2"> Uhr
+                                    <input onClick=\'document.getElementById("startday").value="'.$jetzt['tag'].'";
+                                                     document.getElementById("startmonth").value="'.$jetzt['monat'].'";
+                                                     document.getElementById("startyear").value="'.$jetzt['jahr'].'";
+                                                     document.getElementById("starthour").value="'.$jetzt['stunde'].'";
+                                                     document.getElementById("startminute").value="'.$jetzt['minute'].'";\' class="button" type="button" value="Jetzt">
                                     <input onClick=\'changeDate ("startday", "startmonth", "startyear", "starthour", "startminute", "7", "0", "0", "0", "0");\' class="button" type="button" value="+1 Woche">
                                     <input onClick=\'changeDate ("startday", "startmonth", "startyear", "starthour", "startminute", "-7", "0", "0", "0", "0");\' class="button" type="button" value="-1 Woche">
                                 </td>
@@ -93,17 +93,17 @@ else
                                     <font class="small">Bild soll angezeigt werden bis</font>
                                 </td>
                                 <td class="config" valign="top" colspan="2">
-                                    <input id="endday"  class="text" size="1" value="'.$_POST[endtag].'" name="endtag" maxlength="2"> .
-                                    <input id="endmonth" class="text" size="1" value="'.$_POST[endmonat].'" name="endmonat" maxlength="2"> .
-                                    <input id="endyear" class="text" size="3" value="'.$_POST[endjahr].'" name="endjahr" maxlength="4">
+                                    <input id="endday"  class="text" size="1" value="'.$_POST['endtag'].'" name="endtag" maxlength="2"> .
+                                    <input id="endmonth" class="text" size="1" value="'.$_POST['endmonat'].'" name="endmonat" maxlength="2"> .
+                                    <input id="endyear" class="text" size="3" value="'.$_POST['endjahr'].'" name="endjahr" maxlength="4">
                                     <font class="small"> um </font>
-                                    <input id="endhour" class="text" size="1" value="'.$_POST[endstunde].'" name="endstunde" maxlength="2"> :
-                                    <input id="endminute" class="text" size="1" value="'.$_POST[endmin].'" name="endmin" maxlength="2"> Uhr
-                                    <input onClick=\'document.getElementById("endday").value="'.$jetzt[tag].'";
-                                                     document.getElementById("endmonth").value="'.$jetzt[monat].'";
-                                                     document.getElementById("endyear").value="'.$jetzt[jahr].'";
-                                                     document.getElementById("endhour").value="'.$jetzt[stunde].'";
-                                                     document.getElementById("endminute").value="'.$jetzt[minute].'";\' class="button" type="button" value="Jetzt">
+                                    <input id="endhour" class="text" size="1" value="'.$_POST['endstunde'].'" name="endstunde" maxlength="2"> :
+                                    <input id="endminute" class="text" size="1" value="'.$_POST['endmin'].'" name="endmin" maxlength="2"> Uhr
+                                    <input onClick=\'document.getElementById("endday").value="'.$jetzt['tag'].'";
+                                                     document.getElementById("endmonth").value="'.$jetzt['monat'].'";
+                                                     document.getElementById("endyear").value="'.$jetzt['jahr'].'";
+                                                     document.getElementById("endhour").value="'.$jetzt['stunde'].'";
+                                                     document.getElementById("endminute").value="'.$jetzt['minute'].'";\' class="button" type="button" value="Jetzt">
                                     <input onClick=\'changeDate ("endday", "endmonth", "endyear", "endhour", "endminute", "7", "0", "0", "0", "0");\' class="button" type="button" value="+1 Woche">
                                     <input onClick=\'changeDate ("endday", "endmonth", "endyear", "endhour", "endminute", "-7", "0", "0", "0", "0");\' class="button" type="button" value="-1 Woche">
                                 </td>
@@ -112,7 +112,7 @@ else
                             <tr>
                                 <td></td>
                                 <td align="left" colspan="2">
-                                    <input class="button" type="submit" value="Hinzufügen">
+                                    <input class="button" type="submit" value="Hinzuf&uuml;gen">
                                 </td>
                             </tr>
                         </table>
