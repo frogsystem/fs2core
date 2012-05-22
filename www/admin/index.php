d7289f32eeaf2802339f54eda0db8ec48d35881c
Thoronador <thoronador@users.sourceforge.net>
2012-05-22 20:14:50 +0200

    more pedantic stuff

--- www/admin/index.php
+++ www/admin/index.php
@@ -1,36 +1,36 @@
 <?php
 /* FS2 PHP Init */
 set_include_path('.');
-define('FS2_ROOT_PATH', "./../", true);
-require_once(FS2_ROOT_PATH . "includes/phpinit.php");
+define('FS2_ROOT_PATH', './../', true);
+require_once(FS2_ROOT_PATH . 'includes/phpinit.php');
 phpinit();
 /* End of FS2 PHP Init */
 
 
 // inlcude files
-require_once(FS2_ROOT_PATH . "includes/functions.php");
-require_once(FS2_ROOT_PATH . "includes/adminfunctions.php");
-require_once(FS2_ROOT_PATH . "includes/imagefunctions.php");
-require_once(FS2_ROOT_PATH . "includes/templatefunctions.php");
-require_once(FS2_ROOT_PATH . "includes/indexfunctions.php");
-require_once(FS2_ROOT_PATH . "libs/class_GlobalData.php");
-require_once(FS2_ROOT_PATH . "login.inc.php");
+require_once(FS2_ROOT_PATH . 'includes/functions.php');
+require_once(FS2_ROOT_PATH . 'includes/adminfunctions.php');
+require_once(FS2_ROOT_PATH . 'includes/imagefunctions.php');
+require_once(FS2_ROOT_PATH . 'includes/templatefunctions.php');
+require_once(FS2_ROOT_PATH . 'includes/indexfunctions.php');
+require_once(FS2_ROOT_PATH . 'libs/class_GlobalData.php');
+require_once(FS2_ROOT_PATH . 'login.inc.php');
 
 //Include Library-Classes
-require_once(FS2_ROOT_PATH . "libs/class_template.php");
-require_once(FS2_ROOT_PATH . "libs/class_fileaccess.php");
-require_once(FS2_ROOT_PATH . "libs/class_lang.php");
-require_once(FS2_ROOT_PATH . "libs/class_adminpage.php");
+require_once(FS2_ROOT_PATH . 'libs/class_template.php');
+require_once(FS2_ROOT_PATH . 'libs/class_fileaccess.php');
+require_once(FS2_ROOT_PATH . 'libs/class_lang.php');
+require_once(FS2_ROOT_PATH . 'libs/class_adminpage.php');
 
 // Constructor Calls
-setTimezone($FD->cfg("timezone"));
+setTimezone($FD->cfg('timezone'));
 
 //Include Phrases-Files
-require(FS2_ROOT_PATH . "lang/de_DE/admin/admin_phrases_de.php");
-$TEXT['admin']    = new lang($global_config_arr['language_text'], "admin");
-$TEXT['frontend'] = new lang($global_config_arr['language_text'], "frontend");
-$TEXT['template'] = new lang($global_config_arr['language_text'], "template");
-$TEXT['menu']     = new lang($global_config_arr['language_text'], "menu");
+require(FS2_ROOT_PATH . 'lang/de_DE/admin/admin_phrases_de.php');
+$TEXT['admin']    = new lang($global_config_arr['language_text'], 'admin');
+$TEXT['frontend'] = new lang($global_config_arr['language_text'], 'frontend');
+$TEXT['template'] = new lang($global_config_arr['language_text'], 'template');
+$TEXT['menu']     = new lang($global_config_arr['language_text'], 'menu');
 
 
 ######################
@@ -66,12 +66,12 @@ $go = $sql->escape($_REQUEST['go']);
 
 // get page-data from database
 $acp_arr = $sql->getRow(
-    array('P' => "admin_cp", 'G' => "admin_groups"),
+    array('P' => 'admin_cp', 'G' => 'admin_groups'),
     array(
-        array('COL' => "page_id", 'FROM' => "P"),
-        array('COL' => "page_file", 'FROM' => "P"),
-        array('COL' => "group_id", 'FROM' => "P"),
-        array('COL' => "menu_id", 'FROM' => "G"),
+        array('COL' => 'page_id', 'FROM' => 'P'),
+        array('COL' => 'page_file', 'FROM' => 'P'),
+        array('COL' => 'group_id', 'FROM' => 'P'),
+        array('COL' => 'menu_id', 'FROM' => 'G'),
     ),
     array('W' => "P.`group_id` = G.`group_id` AND P.`page_id` = '".$go."' AND P.`page_int_sub_perm` != 1")
 );
@@ -82,54 +82,54 @@ if (!empty($acp_arr)) {
     // if page is start page
     if ($acp_arr['group_id'] == -1) {
         $acp_arr['menu_id'] = $acp_arr['page_file'];
-        $acp_arr['page_file'] = $acp_arr['page_id'].".php";
+        $acp_arr['page_file'] = $acp_arr['page_id'].'.php';
     }
-    
+
     //if popup
-    if ($acp_arr['group_id'] == "popup") {
+    if ($acp_arr['group_id'] == 'popup') {
         define('POPUP', true);
-        $title = $TEXT['menu']->get("page_title_".$acp_arr['page_id']);
+        $title = $TEXT['menu']->get('page_title_'.$acp_arr['page_id']);
     } else {
         define('POPUP', false);
-        $title = $TEXT['menu']->get("group_".$acp_arr['group_id'])." &#187; ".$TEXT['menu']->get("page_title_".$acp_arr['page_id']);
+        $title = $TEXT['menu']->get('group_'.$acp_arr['group_id']).' &#187; '.$TEXT['menu']->get('page_title_'.$acp_arr['page_id']);
     }
 
     // get the page-data
     $PAGE_DATA_ARR = createpage($title, has_perm($acp_arr['page_id']), $acp_arr['page_file'], $acp_arr['menu_id']);
-    
+
     // initialise templatesystem
     $adminpage = new adminpage($acp_arr['page_file']);
-    
+
     // Get Special Page Lang-Text-Files
-    $page_lang = new lang($global_config_arr['language_text'], "admin/".substr($acp_arr['page_file'], 0, -4));
+    $page_lang = new lang($global_config_arr['language_text'], 'admin/'.substr($acp_arr['page_file'], 0, -4));
     $FD->setPageText($page_lang);
     if (!isset($TEXT['page'])) { //TODO old
         $TEXT['page'] = $page_lang;
     }
     unset ($page_lang);
-    
+
 } else {
     $PAGE_DATA_ARR['created'] = false;
     define('POPUP', false);
 }
 
 // logout
-if ( $PAGE_DATA_ARR['created'] === false && $go == "logout" ) {
-    setcookie ("login", "", time() - 3600, "/");
+if ( $PAGE_DATA_ARR['created'] === false && $go == 'logout' ) {
+    setcookie ('login', '', time() - 3600, '/');
     $_SESSION = array();
-    $PAGE_DATA_ARR = createpage($TEXT['menu']->get("admin_logout_text"), true, 'admin_logout.php', "dash");
+    $PAGE_DATA_ARR = createpage($TEXT['menu']->get('admin_logout_text'), true, 'admin_logout.php', 'dash');
 }
 
 // login
-elseif ( $PAGE_DATA_ARR['created'] === false && ($go == "login" || empty($go)) ) {
-    $go = "login";
-    $PAGE_DATA_ARR = createpage($TEXT['menu']->get("admin_login_text"), true, 'admin_login.php', "dash");
+elseif ( $PAGE_DATA_ARR['created'] === false && ($go == 'login' || empty($go)) ) {
+    $go = 'login';
+    $PAGE_DATA_ARR = createpage($TEXT['menu']->get('admin_login_text'), true, 'admin_login.php', 'dash');
 }
 
 // error
 elseif ( $PAGE_DATA_ARR['created'] === false ) {
-    $go = "404";
-    $PAGE_DATA_ARR = createpage($TEXT['menu']->get("admin_error_page_title"), true, 'admin_404.php', "error");
+    $go = '404';
+    $PAGE_DATA_ARR = createpage($TEXT['menu']->get('admin_error_page_title'), true, 'admin_404.php', 'error');
 }
 
 
@@ -153,21 +153,21 @@ echo'<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.
 <head>
     <title>Frogsystem 2 - '.$PAGE_DATA_ARR['title'].'</title>
     <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-    
+
     <link rel="stylesheet" type="text/css" href="css/admin_old.css">
     <link rel="stylesheet" type="text/css" href="css/editor.css">
-    
+
     <link rel="stylesheet" type="text/css" href="css/main.css"> 
     <link rel="stylesheet" type="text/css" href="css/noscript.css" id="noscriptcss">
-    
+
     <link rel="shortcut icon" href="icons/favicon.ico">  
-    
+
     <script src="../resources/jquery/jquery.min.js" type="text/javascript"></script>
     <script src="../resources/jquery/jquery-ui.min.js" type="text/javascript"></script>
     <script src="functions.js" type="text/javascript"></script>
     <script src="../includes/js_functions.js" type="text/javascript"></script>
     <script src="js/admin.js" type="text/javascript"></script>
-    
+
     <link rel="stylesheet" type="text/css" href="../resources/colorpicker/css/colorpicker.css">
     <script type="text/javascript" src="../resources/colorpicker/js/colorpicker.js"></script>  
     <script type="text/javascript" src="js/colorpicker.js"></script>    
@@ -194,7 +194,7 @@ echo'
          version '.$global_config_arr['version'].'
      </div>
      <div id="head_link">
-         <a href="'.$global_config_arr['virtualhost'].'" target="_self" class="head_link">» '.$TEXT['menu']->get("admin_link_to_page").'</a>
+         <a href="'.$global_config_arr['virtualhost'].'" target="_self" class="head_link">» '.$TEXT['menu']->get('admin_link_to_page').'</a>
      </div>
 </div>
 <!-- /Page Head -->';
@@ -230,13 +230,13 @@ echo '
 <div id="topmenu_login">';
 
 if (is_authorized()) {
-    $log_link = "logout";
-    $log_image = "logout.gif";
-    $log_text = $TEXT['menu']->get("admin_logout_text");
+    $log_link = 'logout';
+    $log_image = 'logout.gif';
+    $log_text = $TEXT['menu']->get('admin_logout_text');
 } else {
-    $log_link = "login";
-    $log_image = "login.gif";
-    $log_text = $TEXT['menu']->get("admin_login_text");
+    $log_link = 'login';
+    $log_image = 'login.gif';
+    $log_text = $TEXT['menu']->get('admin_login_text');
 }
 
 echo'
@@ -263,7 +263,7 @@ echo'
     <!-- Left Menu -->
     <div id="navi_container">';
 
-if ($template_navi == "") {
+if ($template_navi == '') {
     $template_navi = '
         <div class="leftmenu top" style="height:43px;">
             <img src="icons/arrow.gif" alt="->">&nbsp;<b>Hallo Admin!</b>
@@ -295,7 +295,7 @@ echo '
     <div id="content_container">';
 
 ob_start();
-require(FS2_ROOT_PATH . "admin/".$PAGE_DATA_ARR['file']);
+require(FS2_ROOT_PATH . 'admin/'.$PAGE_DATA_ARR['file']);
 $content = ob_get_clean();
 
 $top = '<h2 class="cb-text">('.$PAGE_DATA_ARR['title'].')</h2>';
@@ -323,7 +323,7 @@ echo '
 $JUST_CONTENT = false;
 
 ob_start();
-require(FS2_ROOT_PATH . "admin/".$PAGE_DATA_ARR['file']);
+require(FS2_ROOT_PATH . 'admin/'.$PAGE_DATA_ARR['file']);
 $popup = ob_get_clean();
 
 if ($JUST_CONTENT !== true) {
