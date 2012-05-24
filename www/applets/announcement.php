99de48fd43e9cbb33614ee9b9158f49e9dfc53f7
Thoronador <thoronador@users.sourceforge.net>
2012-05-24 02:44:23 +0200

    pedantic fixes for affiliates and announcement applets

--- www/applets/announcement.php
+++ www/applets/announcement.php
@@ -2,9 +2,9 @@
 ///////////////////////////
 //// Show Announcement ////
 ///////////////////////////
-$index = mysql_query ( "
+$index = mysql_query ( '
                         SELECT *
-                        FROM `".$global_config_arr['pref']."announcement`
+                        FROM `'.$global_config_arr['pref']."announcement`
                         WHERE `id` = '1'
 ", $FD->sql()->conn() );
 $ann_arr = mysql_fetch_assoc ( $index );
@@ -12,17 +12,17 @@ $ann_arr = mysql_fetch_assoc ( $index );
 if ( $ann_arr['show_announcement'] != 0 && $ann_arr['activate_announcement'] == 1 ) {
 
     if ( $ann_arr['show_announcement'] == 2 && $global_config_arr['goto'] != $global_config_arr['home_real'] ) {
-        $template == "";
+        $template = '';
     } else {
         $ann_arr['announcement_text'] = fscode ( $ann_arr['announcement_text'], $ann_arr['ann_fscode'], $ann_arr['ann_html'], $ann_arr['ann_para'] );
         // Body Template
         $template = new template();
-        $template->setFile("0_general.tpl");
-        $template->load("ANNOUNCEMENT");
-        $template->tag("announcement_text", $ann_arr['announcement_text'] );
+        $template->setFile('0_general.tpl');
+        $template->load('ANNOUNCEMENT');
+        $template->tag('announcement_text', $ann_arr['announcement_text'] );
         $template = $template->display ();
     }
 } else {
-    $template = "";
+    $template = '';
 }
 ?>
