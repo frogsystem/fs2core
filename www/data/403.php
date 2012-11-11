df06d48d26bbd1798e8a2ab242d5689477fadfe1
Sweil <mail@sweil.de>
2012-11-11 12:35:46 +0100

    fixed styling of "Error XYZ"
    Signed-off-by: Sweil <mail@sweil.de>

--- www/data/403.php
+++ www/data/403.php
@@ -1,5 +1,5 @@
 <?php
 header(http_response_text(403), true, 403);
-$template = sys_message($FD->text('frontend', 'sysmessage'), '<b><span color="red">Error 403:</span></b> '.$FD->text('frontend', 'access_denied').'.');
+$template = sys_message($FD->text('frontend', 'sysmessage'), '<b><span style="color:#FF0000;">Error 403:</span></b> '.$FD->text('frontend', 'access_denied').'.');
 $FD->setConfig('info', 'page_title', 'Error 403');
 ?>
