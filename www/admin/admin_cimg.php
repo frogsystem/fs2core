3ea0e461559a8ad0b29b6ad58673c3e56b119a96
Mainclain <github@mainclain.de>
2014-06-01 19:34:15 +0200

    Content Image Size Fix

--- www/admin/admin_cimg.php
+++ www/admin/admin_cimg.php
@@ -46,7 +46,7 @@ if (isset($_FILES['cimg']) AND (isset($_POST['newname']) OR $_POST['oldname'] ==
 
   if (!image_exists('media/content/',$_POST['newname'])  AND !image_exists('media/content/',$_POST['newname'].'_s'))
   {
-    $upload = upload_img($_FILES['cimg'], 'media/content/', $_POST['newname'], 1024*1024, 9999, 9999);
+    $upload = upload_img($_FILES['cimg'], 'media/content/', $_POST['newname'], 1024*1024*5, 9999, 9999);
     $message = upload_img_notice ( $upload );
     if ($make_thumb) {
       $thumb = create_thumb_from ( image_url ( 'media/content/', $_POST['newname'], FALSE, TRUE ) , $_POST['width'], $_POST['height'] );
