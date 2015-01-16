90f56297d6489d212e99125e54a4f10e871754f2
Moritz Kornher <mail@moritzkornher.de>
2015-01-16 17:38:57 +1300

    moved feeds
    Signed-off-by: Moritz Kornher <mail@moritzkornher.de>

--- www/data/feed.php
+++ www/data/feed.php
@@ -1,6 +1,11 @@
 <?php
-header('Content-type: application/xml');
 require_once(FS2SOURCE . '/libs/class_Feed.php');
-include(FS2SOURCE.'/feeds/'.$_GET['xml'].'.php');
-exit;
+$feed = FS2SOURCE.'/classes/feeds/'.$_GET['xml'].'.php';
+if (file_exists($feed)) {
+    header('Content-type: application/xml');
+    include($feed);
+    exit;
+} else {
+    $template = sys_message($FD->text('frontend', 'systemmessage'), $FD->text('frontend', 'file_not_found'), 404 );
+}
 ?>
